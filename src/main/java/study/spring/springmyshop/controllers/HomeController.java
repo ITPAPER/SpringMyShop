23ddbe7c0fa75e90403f05a07d093203b6c920a2
ju <j.purplerose@gmail.com>
2020-05-05 14:33:29 +0900

    inc파일들 jstl 참조 추가

--- src/main/java/study/spring/springmyshop/controllers/HomeController.java
+++ src/main/java/study/spring/springmyshop/controllers/HomeController.java
@@ -9,23 +9,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import study.spring.springmyshop.model.Members;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Handles requests for the application home page.
  */
+@Slf4j
 @Controller
 public class HomeController {
-
-    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
     /**
      * Simply selects the home view to render by returning its name.
      */
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Locale locale, Model model) {
-        Members members = new Members();
-
+        log.debug("Home Controller");
         return "home";
     }
 
